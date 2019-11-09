028f09effed40104610fb463880616df7f3f5d72
Damodar Lohani <dlohani48@gmail.com>
2019-11-09 11:06:24 +0545

    add worklog setup

---
+++ lib/features/stat/presentation/pages/add_work_log.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AddWorkLogPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add new log entry'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
