38c17c8cddb4b81861be9ff07fc84a86a98de07c
Damodar Lohani <dlohani48@gmail.com>
2019-11-10 15:43:24 +0545

    date time functions

--- lib/core/presentation/res/functions.dart
+++ lib/core/presentation/res/functions.dart
@@ -8,3 +8,19 @@ String durationToHMString(Duration duration) {
     
     return '${hoursRemaining}h ${minutesRemaining}m';
 }
+
+final DateTime _today = dayFromDate(DateTime.now());
+DateTime get today=> _today;
+DateTime dayFromDate(date) => DateTime(date.year,date.month,date.day);
+
+DateTime addDay(date, days) => DateTime(date.year,date.month,date.day + days);
+DateTime subDay(date, days) => DateTime(date.year,date.month,date.day - days);
+
+DateTime firstDayOfYear(date) => DateTime(date.year, 1, 1);
+DateTime lastDayOfYear(date) => DateTime(date.year, 12, 31);
+DateTime nextYear(date) => DateTime(date.year+1, date.month, date.day);
+DateTime prevYear(date) => DateTime(date.year-1, date.month, date.day);
+DateTime prevDay(date) => DateTime(date.year, date.month, date.day-1);
+DateTime nextDay(date) => DateTime(date.year, date.month, date.day+1);
+DateTime beginingOfDay(date) => DateTime(date.year,date.month,date.day,0,0,0);
+DateTime endOfDay(date) => DateTime(date.year,date.month,date.day,24,0,0);
