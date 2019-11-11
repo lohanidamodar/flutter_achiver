ce4d29e7146e4ac0b34bcdf1affaa9214c2e1f08
Damodar Lohani <dlohani48@gmail.com>
2019-11-11 12:07:24 +0545

    splash page background

--- lib/features/auth/presentation/pages/splash.dart
+++ lib/features/auth/presentation/pages/splash.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_achiver/core/presentation/res/assets.dart';
 
 class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("Splash Screen"),
+      child: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.pink,Colors.indigo],
+          ),
+          image: DecorationImage(
+            image: AssetImage(welcomeBg),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(Colors.black26, BlendMode.dstATop)
+          )
+        ),
+        child: CircularProgressIndicator(),
       ),
     );
   }
