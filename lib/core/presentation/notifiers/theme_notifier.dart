76bfa2241821393d5ad889131605e98f2db3ec89
Damodar Lohani <dlohani48@gmail.com>
2019-11-15 17:16:04 +0545

    persist theme choice

--- lib/core/presentation/notifiers/theme_notifier.dart
+++ lib/core/presentation/notifiers/theme_notifier.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_achiver/core/presentation/res/colors.dart' as prefix0;
+import 'package:flutter_achiver/core/presentation/res/colors.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeNotifier extends ChangeNotifier {
+  final String key = "dark_theme";
+  SharedPreferences prefs;
   ThemeData _lightTheme=ThemeData(
-    scaffoldBackgroundColor: prefix0.bgColor,
+    scaffoldBackgroundColor: bgColor,
     brightness: Brightness.light,
     primarySwatch: Colors.pink,
     inputDecorationTheme: InputDecorationTheme(
@@ -26,11 +29,28 @@ class ThemeNotifier extends ChangeNotifier {
   ThemeNotifier() {
     currentTheme = _darkTheme;
     darkTheme = true;
+    _loadFromPrefs();
   }
 
   toggleTheme() {
     currentTheme = darkTheme ? _lightTheme : _darkTheme;
     darkTheme = !darkTheme;
+    _saveToPrefs();
     notifyListeners();
+  }
+
+  _loadFromPrefs() async {
+    await _initPrefs();
+    darkTheme = prefs.getBool(key) ?? true;
+    currentTheme = darkTheme ? _darkTheme : _lightTheme;
+    notifyListeners();
+  }
+  _saveToPrefs() async {
+    await _initPrefs();
+    prefs.setBool(key, darkTheme);
+  }
+  _initPrefs() async {
+    if(prefs== null)
+      prefs = await SharedPreferences.getInstance();
   }
 }
